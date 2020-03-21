2ee43769b8f0340fc47cff82fde27cb6d62e414c
Gonzalo <gonzamartinezok@gmail.com>
2020-03-21 14:12:40 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://GonzaMartinez:URAFpX5RImkBcnf4@cluster0-9ziul.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
